20709fd77fde92ce0ac2e975798d0cf1584c451c
SweetBajebes <59667770+22lejo@users.noreply.github.com>
2021-01-17 17:03:47 -0500

    Delete 2021GitHubTraining directory

--- 2021GitHubTraining/VehicleProject/Motorcycle.java
+++
@@ -1,5 +0,0 @@
-public class Motorcycle extends Vehicle 
-//Try to create a Motorcycle Class, using the Car Class as an example. 
-{
-
-}
--- 2021GitHubTraining/VehicleProject/Truck.java
+++
@@ -1,5 +0,0 @@
-public class Truck extends Vehicle
-//Try to create a Truck Class, using the Car Class as an example. 
-{
-
-}
