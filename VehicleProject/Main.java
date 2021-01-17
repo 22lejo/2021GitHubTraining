12c09fdb94b478e451dc7f491760bedb5710d841
LejoLevan <JonathanVonLe@gmail.com>
2021-01-17 16:47:47 -0500

    Final edits on practice task (added another class for groups to work on, and rethought structure)

--- VehicleProject/Main.java
+++ VehicleProject/Main.java
@@ -2,26 +2,28 @@ public class Main {
 
 	public static void main(String[] args)
 	{ 
-		//Car Object
-		Car car = new Car();
-		car.print();
-    System.out.println("***********************************");
+		//Vehicle Object
+		Vehicle vehicle = new Vehicle();
+		vehicle.print();
+    	System.out.println("***********************************");
 		
 		//Toyota Object but called the print method from car class, not the print method in Toyota class
-		Toyota toyotaCar = new Toyota();
-    System.out.println("The car details are from Car Class.");
+		Car toyotaCar = new Car();
+    	System.out.println("The car details are from Car Class.");
 		toyotaCar.print();
 		System.out.println("***********************************");
 
 		//Toyota Object with Print method from Toyota Class
-		Toyota toyotaCar2 = new Toyota();
+		Car toyotaCar2 = new Car();
 		toyotaCar2.printDetails();
 		System.out.println("***********************************");
 
 		//Create Toyota Object with parameters
-		Toyota toyotaCar3 = new Toyota("Silver",2005, 22.7, 56800, "Sedan", true);
+		Car toyotaCar3 = new Car("Silver",2005, 22.7, 56800, "Sedan", true);
 		toyotaCar3.printDetails();
 		System.out.println("***********************************");
+
+		//Use the space below to create code to test your Truck class and Motorcycle class
 
 
 	}
